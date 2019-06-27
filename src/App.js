b1e867fa37b7a9a92368f0651c86e02b75680987
asabogal <asabogal.git@gmail.com>
2019-06-26 21:14:43 -0400

    change Home to App + add Helmet for SEO

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Helmet from 'react-helmet'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Helmet>
+          <title>Kon-Tiki Greenport</title>
+          <meta name="description" content="Kon-Tiki Restaurant and Bar at The Gallery Hotel" />
+          <meta name="keywords" cpntent="greenport, restaurant, bar, gallery hotel, gallery, hotel" />
+        </Helmet>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './Home'
+import App from './App'
 import './index.css';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
