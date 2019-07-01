b1c2520d4290a5455df31a80bcc338fbdc30514d
asabogal <asabogal.git@gmail.com>
2019-06-30 22:42:21 -0400

    add header to landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Navigation from '../components/landing/Navigation'
 import LandingImage from '../components/landing/LandingImage'
+import Header from '../components/landing/Header'
 import './style.css'
 
 class LandingPage extends Component {
   render() {
     return (
       <div className="landing-page">
+        <Header/> 
         <div className="landing-flex">
           <Navigation/>
           <LandingImage/>
