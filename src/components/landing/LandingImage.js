22b021f100649ce18e2013fb6585fb37b7e78705
asabogal <asabogal.git@gmail.com>
2019-07-02 16:00:50 -0400

    add initial animations

--- src/components/landing/LandingImage.js
+++ src/components/landing/LandingImage.js
@@ -4,9 +4,7 @@ import gif from '../../resources/images/kt_gif edit.gif'
 
 const LandingImage = () => {
   return (
-    <div className="img-container">
       <img className="landing-img" src={gif}/>
-    </div>
   );
 };
 
--- src/components/landing/Navigation.js
+++ src/components/landing/Navigation.js
@@ -5,10 +5,10 @@ class Navigation extends Component {
   render() {
     return (
       <div className="navigation">
-        <div>HOME</div>
-        <div>MENU</div>
-        <div>RESERVE</div>
-        <div>CONTACT</div>
+        <div className="landing-home">HOME</div>
+        <div className="landing-menu">MENU</div>
+        <div className="landing-reserve">RESERVE</div>
+        <div className="landing-contact">CONTACT</div>
       </div>
     );
   }
