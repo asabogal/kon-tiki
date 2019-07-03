f7cf2db5c1836297c723c806d4665616312d6de2
asabogal <asabogal.git@gmail.com>
2019-07-03 01:45:03 -0400

    change Header comp to class

--- src/components/landing/Header.js
+++ src/components/landing/Header.js
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './style.css'
 import instagram from '../../resources/icons/instagram.png'
 import facebook from '../../resources/icons/facebook.png'
 import twitter from '../../resources/icons/twitter.png'
 
-const Header = () => {
-  return (
-    <div className="landing-header">
-      <a href="https://www.instagram.com/kontiki.gp/">
-        <img src={instagram} alt="instagram"/>
-      </a>
-      
-      <a href="https://twitter.com/kontikiny?lang=en">
-        <img src={facebook} alt="facebook"/>
-      </a>
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isOpen: false };
+  }
+
+  renderIcons = () => {
+    return (
+      <div>
+        <a href="https://www.instagram.com/kontiki.gp/">
+          <img src={instagram} alt="instagram"/>
+        </a>
     
-      <a href="https://twitter.com/kontikiny?lang=en">
-        <img src={twitter} alt="twitter"/>
-      </a>
-    </div>
-  );
-};
+        <a href="https://twitter.com/kontikiny?lang=en">
+          <img src={facebook} alt="facebook"/>
+        </a>
+    
+        <a href="https://twitter.com/kontikiny?lang=en">
+          <img src={twitter} alt="twitter"/>
+        </a>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div className="landing-header">    
+        {this.renderIcons()}
+      </div>  
+    );
+  }
+}
 
 export default Header;
