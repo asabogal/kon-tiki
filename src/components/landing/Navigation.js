2fe5c0f3ddb78a3e315c716b5950cd2f98d11c9d
asabogal <asabogal.git@gmail.com>
2019-07-10 00:22:41 -0400

    add Router Links and fix style children

--- src/components/landing/Navigation.js
+++ src/components/landing/Navigation.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 import './style.css'
 
 class Navigation extends Component {
   render() {
     return (
-      <div className="navigation">
-        <div className="landing-home">HOME</div>
-        <div className="landing-menu">MENU</div>
-        <div className="landing-reserve">RESERVE</div>
-        <div className="landing-contact">CONTACT</div>
+      <div className="landing-navigation">
+        <a> <Link to="/">HOME</Link> </a>
+        <a> <Link to="/">MENU</Link> </a>
+        <a> <Link to="/">RESERVE</Link> </a>
+        <a> <Link to="/">CONTACT</Link> </a>
       </div>
     );
   }
