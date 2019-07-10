a629eab01fba3b52546950a6a1bb8dec18368873
asabogal <asabogal.git@gmail.com>
2019-07-10 00:25:16 -0400

    move navigation folders to landing folder + fix styling

---
+++ src/components/landing/navigation/MobileMenu.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import './style.css'
+
+const MobileMenu = (props) => {
+
+  const handleClick = () => {
+    props.toggle()
+    document.querySelector('.landing-img').style.display = 'block'
+    document.querySelector('.navigation').style.display = 'flex'
+  }
+
+  return (
+    <div className="overlay" onClick={handleClick}>
+      <div className="overlay-content">
+        <a> <Link to="/">HOME</Link> </a>
+        <a> <Link to="/">MENU</Link> </a>
+        <a> <Link to="/">RESERVE</Link> </a>
+        <a> <Link to="/">CONTACT</Link> </a>
+      </div>
+    </div>
+  );
+};
+
+export default MobileMenu;
